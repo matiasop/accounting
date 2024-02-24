c15c4a3f0c9928e16e3f166534a4e933adc00a90
matias-ovalle-toteat <matias@toteat.com>
2024-02-23 23:22:16 -0300

    chore: initialize project

---
+++ accounting/settings.py
@@ -0,0 +1,66 @@
+import enum
+import os
+from pathlib import Path
+from tempfile import gettempdir
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from yarl import URL
+
+TEMP_DIR = Path(gettempdir())
+
+
+class LogLevel(str, enum.Enum):  # noqa: WPS600
+    """Possible log levels."""
+
+    NOTSET = "NOTSET"
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    FATAL = "FATAL"
+
+
+class Settings(BaseSettings):
+    """
+    Application settings.
+
+    These parameters can be configured
+    with environment variables.
+    """
+
+    host: str = "127.0.0.1"
+    port: int = 8000
+    # quantity of workers for uvicorn
+    workers_count: int = 1
+    # Enable uvicorn reloading
+    reload: bool = False
+
+    # Current environment
+    environment: str = "dev"
+
+    log_level: LogLevel = LogLevel.INFO
+    users_secret: str = os.getenv("USERS_SECRET", "")
+    # Variables for the database
+    db_file: Path = TEMP_DIR / "db.sqlite3"
+    db_echo: bool = False
+
+    @property
+    def db_url(self) -> URL:
+        """
+        Assemble database URL from settings.
+
+        :return: database URL.
+        """
+        return URL.build(
+            scheme="sqlite+aiosqlite",
+            path=f"///{self.db_file}",
+        )
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_prefix="ACCOUNTING_",
+        env_file_encoding="utf-8",
+    )
+
+
+settings = Settings()
